c415f7363988c2f884f693ae06f5a702e9ae5eb4
Ashley Smith <ashley@aas.sh>
2019-02-26 04:13:15 +0000

    Getting basics of game implemented

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@
 module Main where
 
 import Prelude hiding (replicate)
-import Data.Sequence (replicate)
+import Data.Sequence (replicate, adjust, index)
 import Data.Char (toUpper, ord)
 import Text.Read (readMaybe)
 import Data.Maybe (isNothing, fromJust)
@@ -23,19 +23,28 @@ boardSize = (11, 11)
 -- Create a new game state
 newGame :: GameState
 newGame = GameState newBoard P1
-  where newBoard = replicate size Empty
+  where newBoard = Board $ replicate size Empty
         size = let (w, h) = boardSize in w * h
 
 -- Game loop of game
 gameLoop :: GameState -> IO ()
-gameLoop !gs@(GameState board turn) = do
-  coords <- processInput turn
-  print coords
-  gameLoop gs
+gameLoop !gs = do
+  newGS <- takePlayerTurn gs
+  gameLoop newGS
+
+-- How a player will make moves
+takePlayerTurn :: GameState -> IO GameState
+takePlayerTurn gs = do
+  coords <- processInput gs
+  case tryMakeMove gs coords of
+    Just newGS -> pure newGS
+    _ -> do
+      putStrLn "Error: Invalid move. Please re-evaluate strategy and try again."
+      takePlayerTurn gs
 
 -- Take input of the current player and coords or error
-processInput :: Player -> IO (Int, Int)
-processInput turn = do
+processInput :: GameState -> IO (Int, Int)
+processInput gs@(GameState board turn) = do
   putStr $ show turn ++ " - Please make your move eg. A0: "
   input <- getLine
   hFlush stdout
@@ -43,7 +52,7 @@ processInput turn = do
     Left coords -> pure coords
     Right msg -> do
       putStrLn msg
-      processInput turn
+      processInput gs
 
 -- Converts A0 into 00
 convertStrToCoords :: String -> Either (Int, Int) String
@@ -58,3 +67,22 @@ convertStrToCoords (x : y : _)
   where x' = ord (toUpper x) - ord 'A'
         y' = readMaybe [y]
 convertStrToCoords input = Right $ "Error: Invalid input \"" ++ input ++ "\"."
+
+-- Check if the tile at coords is unoccupied
+tryMakeMove :: GameState -> (Int, Int) -> Maybe GameState
+tryMakeMove gs (x, y) = 
+  case index b i of
+    Empty -> Just newGS
+    _ -> Nothing
+  where t = turn gs
+        Board b = board gs
+        i = y * (fst boardSize) + x
+        newB = adjust (const (Stone t)) i b
+        newT P1 = P2
+        newT P2 = P1
+        newGS = 
+          GameState 
+            { board = Board newB
+            , turn = newT t
+            }
+
--- src/Types.hs
+++ src/Types.hs
@@ -2,11 +2,11 @@ module Types
 ( GameState (..)
 , Player (..)
 , Tile (..)
-, Board
+, Board (..)
 ) where
 
 -- List that allows for better insertion
-import Data.Sequence
+import Data.Sequence hiding (Empty)
 
 -- A GameState which can be represented
 data GameState = 
@@ -17,11 +17,21 @@ data GameState =
 
 -- 2 Players in the game
 data Player = P1 | P2
-  deriving (Eq, Show)
+  deriving (Eq)
+
+instance Show Player where
+  show P1 = "Player 1"
+  show P2 = "Player 2"
 
 -- A Tile is just a representation of a player's moves
 data Tile = Empty | Stone Player
   deriving (Eq)
 
+-- How a stone is drawn on the board
+instance Show Tile where
+  show Empty = "."
+  show (Stone P1) = "1"
+  show (Stone P2) = "2"
+
 -- A Board is made up of tiles
-type Board = Seq Tile
+newtype Board = Board (Seq Tile)
